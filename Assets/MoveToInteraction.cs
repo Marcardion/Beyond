1f17b7eea3d6fca61df6185cdc15e2aae2b4a897
Luiz Marcelo <marcardion@gmail.com>
2017-06-08 22:45:06 -0400

    Colocando o robô com animações + controlar o movimento do robô

---
+++ Assets/MoveToInteraction.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+public class MoveToInteraction : MonoBehaviour {
+
+	[SerializeField] private VRInteractiveItem m_InteractiveItem;
+	[SerializeField] private Reticle reticle;
+	[SerializeField] private Unit_Movement unit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnEnable()
+	{
+		m_InteractiveItem.OnOver += HandleOver;
+		m_InteractiveItem.OnOut += HandleOut;
+		m_InteractiveItem.OnClick += HandleClick;
+		m_InteractiveItem.OnDoubleClick += HandleDoubleClick;
+	}
+
+
+	private void OnDisable()
+	{
+		m_InteractiveItem.OnOver -= HandleOver;
+		m_InteractiveItem.OnOut -= HandleOut;
+		m_InteractiveItem.OnClick -= HandleClick;
+		m_InteractiveItem.OnDoubleClick -= HandleDoubleClick;
+	}
+
+
+	//Handle the Over event
+	private void HandleOver()
+	{
+		
+	}
+
+
+	//Handle the Out event
+	private void HandleOut()
+	{
+		//raycaster.OnRaycasthit -= SetTarget;
+	}
+
+
+	//Handle the Click event
+	private void HandleClick()
+	{
+		SetTarget (reticle.ReticleTransform);
+	}
+
+	private void SetTarget(Transform next_target)
+	{
+		unit.target = next_target.position;
+	}
+
+
+	//Handle the DoubleClick event
+	private void HandleDoubleClick()
+	{
+		
+	}
+}
---
+++ Assets/Unit_Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit_Movement : MonoBehaviour {
+
+	private NavMeshAgent my_agent;
+	private Animator my_animator;
+
+	public Vector3 target;
+
+	// Use this for initialization
+	void Start () {
+
+		my_agent = GetComponent<NavMeshAgent> ();
+		my_animator = GetComponentInChildren<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		my_agent.SetDestination (target);
+
+		// Colocar Animacão aqui
+		if (my_agent.velocity != Vector3.zero) {
+			my_animator.SetBool ("Is_Walking", true);
+		} else 
+		{
+			my_animator.SetBool ("Is_Walking", false);
+		}
+	}
+
+	public void SetTarget(Vector3 next_target)
+	{
+		next_target = target;
+	}
+}
