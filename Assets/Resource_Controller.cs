3e186c00e47788a83972b696807ae1bf26f72aee
Luiz Marcelo <marcardion@gmail.com>
2017-06-14 14:10:25 -0400

    Inclusão do Resource_Manager.cs e Resource_Controller.cs para gerenciar os coletaveis do jogo

---
+++ Assets/Resource_Controller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Resource_Controller : MonoBehaviour {
+
+	[SerializeField] private ResourceType m_ResourceType;
+	private Resource_Manager resourceManager;
+	private TextMeshProUGUI textMesh;
+
+	// Use this for initialization
+	void Awake () {
+
+		resourceManager = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<Resource_Manager> ();
+
+		textMesh = GetComponent<TextMeshProUGUI> ();
+	}
+
+	//TODO Implementar o sistema de Event para reduzir custo de processamento.
+
+	private void OnEnable()
+	{
+		resourceManager.OnUpdate += UpdateText;
+	}
+
+	private void OnDisable()
+	{
+		resourceManager.OnUpdate -= UpdateText;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		UpdateText();
+	}
+
+	public void UpdateText()
+	{
+		int temp = 0;
+
+		switch (m_ResourceType) 
+		{
+		case ResourceType.Biomass:
+			temp = resourceManager.GetCurrentBiomass ();
+			break;
+		case ResourceType.Gas:
+			temp = resourceManager.GetCurrentGas ();
+			break;
+		case ResourceType.Crystal:
+			temp = resourceManager.GetCurrentCrystal ();
+			break;
+		}
+
+		textMesh.text = temp.ToString ();
+	}
+}
---
+++ Assets/Resource_Manager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType {Gas, Biomass, Crystal};
+
+public class Resource_Manager : MonoBehaviour 
+{
+
+	[SerializeField] private int gasCounter;
+	[SerializeField] private int biomassCounter;
+	[SerializeField] private int crystalCounter;
+
+	private int resourceLimit = 999;
+
+	public event Action OnUpdate;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public int GetCurrentGas()
+	{
+		return gasCounter;
+	}
+
+	public int GetCurrentBiomass()
+	{
+		return biomassCounter;
+	}
+
+	public int GetCurrentCrystal()
+	{
+		return crystalCounter;
+	}
+
+	public void IncreaseGas(int numGas)
+	{
+		gasCounter += numGas;
+		if (gasCounter > resourceLimit) 
+		{
+			gasCounter = resourceLimit;
+		}
+		CallUpdate ();
+	}
+
+	public void DecreaseGas(int numGas)
+	{
+		gasCounter -= numGas;
+		CallUpdate ();
+	}
+		
+	public void IncreaseBiomass(int numBiomass)
+	{
+		biomassCounter += biomassCounter;
+		if (biomassCounter > resourceLimit) 
+		{
+			biomassCounter = resourceLimit;
+		}
+		CallUpdate ();
+	}
+
+	public void DecreaseBiomass(int numBiomass)
+	{
+		biomassCounter -= biomassCounter;
+		CallUpdate ();
+	}
+
+	public void IncreaseCrystal(int numCrystal)
+	{
+		crystalCounter += numCrystal;
+		if (crystalCounter > resourceLimit) 
+		{
+			crystalCounter = resourceLimit;
+		}
+		CallUpdate ();
+	}
+		
+	public void DecreaseCrystal(int numCrystal)
+	{
+		crystalCounter -= numCrystal;
+		CallUpdate ();
+	}
+
+	public void CallUpdate()
+	{
+		if (OnUpdate != null) 
+		{
+			OnUpdate ();
+		}
+	}
+}
